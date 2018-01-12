1c21943ee6842c0d6d9f687660c0fe4b76146845
Hustcw <724213922@qq.com>
2018-01-12 06:47:46 +0000

    Finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,13 @@
+<div class="col-md-12">
+  <footer class="footer">
+    <small class="slogon">
+      <img class="brand-icon" src="/logo.png">
+      <a href="http://47.100.18.67/bootstrap/index.html">TA | Program</a>
+    </small>
+    <nav>
+      <ul>
+        <li><a href="{{route('about')}}">关于</a></li>
+      </ul>
+    </nav>
+  </footer>
+</div>
